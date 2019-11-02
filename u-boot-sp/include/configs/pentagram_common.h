d06868005b829073a85be74b441c1c7aa5e4171a
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2019-11-02 21:43:25 +0800

    u-boot-sp: support Banana Pi BPI-F2S (SP7021)

--- u-boot-sp/include/configs/pentagram_common.h
+++ u-boot-sp/include/configs/pentagram_common.h
@@ -41,6 +41,8 @@
 #define CONFIG_SYS_MONITOR_LEN		(512 << 10)
 #endif /* CONFIG_SYS_TEXT_BASE */
 
+#undef CONFIG_SYS_PROMPT
+#define CONFIG_SYS_PROMPT       		"BPI-IoT> "
 #ifdef CONFIG_SPL_BUILD
 #ifndef CONFIG_SYS_UBOOT_START		/* default entry point */
 #define CONFIG_SYS_UBOOT_START		CONFIG_SYS_TEXT_BASE
@@ -167,7 +169,8 @@
  * But now sp_go DO verify magic/hcrc/dcrc in the quick sunplus uIamge header.
  * So the address passed for sp_go must have header in it.
  */
-#define CONFIG_BOOTCOMMAND \
+#undef CONFIG_BOOTCOMMAND2
+#define CONFIG_BOOTCOMMAND2 \
 "echo [scr] bootcmd started; " \
 "md.l ${bootinfo_base} 1; " \
 "if itest.l *${bootinfo_base} == " __stringify(SPI_NOR_BOOT) "; then " \
@@ -242,6 +245,22 @@
 #endif
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
+   "bpiver=1\0" \
+   "bpi=bananapi\0" \
+   "board=bpi-f2s\0" \
+   "chip=SP7201\0" \
+   "service=linux\0" \
+   "scriptaddr=0x1000000\0" \
+   "device=mmc\0" \
+   "partition=1:1\0" \
+   "kernel=uImage\0" \
+   "root=/dev/mmcblk0p2\0" \
+   "debug=7\0" \
+   "bootenv=uEnv.txt\0" \
+   "checksd=fatinfo ${device} 1:1\0" \
+   "loadbootenv=fatload ${device} ${partition} ${scriptaddr} ${bpi}/${board}/${service}/${bootenv} || fatload ${device} ${partition} ${scriptaddr} ${bootenv}\0" \
+   "boot_normal=if run checksd; then echo Boot from ${device} ; setenv partition 1:1; else echo Boot from eMMC ; setenv partition 0:1 ; fi; if run loadbootenv; then echo Loaded environment from ${bootenv}; env import -t ${scriptaddr} ${filesize}; fi; run uenvcmd; fatload mmc 1:1 ${loadaddr} ${bpi}/${board}/${service}/${kernel}; bootr\0" \
+   "boot_user=echo Boot from USB ; usb start ; setenv device usb ; setenv partition 0:1 ; fi; if run loadbootenv; then echo Loaded environment from ${bootenv}; env import -t ${scriptaddr} ${filesize}; fi; run usercmd; fatload mmc 1:1 ${loadaddr} ${bpi}/${board}/${service}/${kernel}; bootr\0" \
 "bootinfo_base="		__stringify(SP_BOOTINFO_BASE) "\0" \
 "addr_src_kernel="		__stringify(CONFIG_SRCADDR_KERNEL) "\0" \
 "addr_src_dtb="			__stringify(CONFIG_SRCADDR_DTB) "\0" \
@@ -302,7 +321,7 @@
 	"cp.l ${addr_src_kernel} ${addr_dst_kernel} ${sz_kernel}; " \
 	dbg_scr("echo sp_go ${addr_dst_kernel} ${addr_dst_dtb}; ") \
 	"sp_go ${addr_dst_kernel} ${addr_dst_dtb}\0" \
-"emmc_boot=mmc read ${addr_tmp_header} ${addr_src_dtb} 0x1; " \
+"emmc_boot=setenv sdmmc_on 0; run boot_normal; mmc read ${addr_tmp_header} ${addr_src_dtb} 0x1; " \
 	"setenv tmpval 0; setexpr tmpaddr ${addr_tmp_header} + 0x4; run be2le; " \
 	"setexpr sz_dtb ${tmpval} + 0x28; " \
 	"setexpr sz_dtb ${sz_dtb} + 0x200; setexpr sz_dtb ${sz_dtb} / 0x200; " \
@@ -369,7 +388,8 @@
 "isp_sdcard=setenv isp_if mmc && setenv isp_dev $sdcard_devid; " \
 	"setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk1p2 rw user_debug=255 rootwait;"\
 	"mmc list; " \
-	"run isp_common; " \
+	"setenv sdmmc_on 1; " \
+	"run boot_normal; run isp_common; " \
 	"\0" \
 "isp_common=setenv isp_ram_addr 0x1000000; " \
 	"fatls $isp_if $isp_dev / ; " \
@@ -431,5 +451,276 @@ mmc read 0x2fffc0 0x1422 0xa ; mmc read 0x307fc0 0x1822 0x30f0 ; sp_go 0x308000 
 #if !defined(CONFIG_SP_SPINAND) && !defined(CONFIG_MMC_SP_EMMC)
 #define SPEED_UP_SPI_NOR_CLK    /* Set CLK based on flash id */
 #endif
+
+
+#define SCRIPT_BOOT \
+	"fileload=${mmctype}load mmc ${devnum}:${mmcpart} " \
+		"${loadaddr} ${mmcfile}\0" \
+	"fileload2=${mmctype}load mmc ${devnum}:2 " \
+		"${loadaddr} ${mmcfile}\0" \
+	"kernload=setenv loadaddr ${kernel_addr_r};" \
+		"setenv mmcfile ${mmckernfile};" \
+		"run fileload\0" \
+	"initrdload=setenv loadaddr ${rdaddr};" \
+		"setenv mmcfile ${mmcinitrdfile};" \
+		"run fileload\0" \
+	"fdtload=setenv loadaddr ${fdtaddr};" \
+		"setenv mmcfile ${mmcfdtfile};" \
+		"run fileload\0" \
+	"fdtload2=setenv loadaddr ${fdtaddr};" \
+		"setenv mmcfile ${mmcfdtfile};" \
+		"run fileload2\0" \
+	"scriptload=setenv loadaddr ${scriptaddr};" \
+		"setenv mmcfile ${mmcscriptfile};" \
+		"run fileload\0" \
+	"scriptboot=echo Running ${mmcscriptfile} from: mmc ${devnum}:${mmcpart} using ${mmcscriptfile};" \
+		"source ${scriptaddr};" \
+
+
+/* Extra environment variables */
+#define SUNPLUS_ENV_SETTINGS	\
+   "bpiver=1\0" \
+   "bpi=bananapi\0" \
+   "board=bpi-f2s\0" \
+   "chip=SP7201\0" \
+   "service=linux\0" \
+   "scriptaddr=0x1000000\0" \
+   "device=mmc\0" \
+   "partition=1:1\0" \
+   "kernel=uImage\0" \
+   "root=/dev/mmcblk0p2\0" \
+   "debug=7\0" \
+   "bootenv=uEnv.txt\0" \
+	"loadaddr=0x82000000\0" \
+	"mmckernfile=boot/zImage\0" \
+	"mmcinitrdfile= boot/uInitrd\0" \
+	"mmcfdtfile=boot/bpi-f2s.dtb\0" \
+	"mmcscriptfile=boot/boot.scr\0" \
+	"mmctype=ext4\0" \
+	"devnum=1\0" \
+	"mmcpart=1\0" \
+	"mmc_bootdev=1\0" \
+   "checksd=fatinfo ${device} 1:1\0" \
+   "loadbootenv=fatload ${device} ${partition} ${scriptaddr} ${bpi}/${board}/${service}/${bootenv} || fatload ${device} ${partition} ${scriptaddr} ${bootenv}\0" \
+   "boot_normal=if run checksd; then echo Boot from ${device} ; setenv partition 1:1; else echo Boot from eMMC ; setenv partition 0:1 ; fi; if run loadbootenv; then echo Loaded environment from ${bootenv}; env import -t ${scriptaddr} ${filesize}; fi; run uenvcmd; fatload mmc 1:1 ${loadaddr} ${bpi}/${board}/${service}/${kernel}; bootr\0" \
+   "boot_user=echo Boot from USB ; usb start ; setenv device usb ; setenv partition 0:1 ; fi; if run loadbootenv; then echo Loaded environment from ${bootenv}; env import -t ${scriptaddr} ${filesize}; fi; run usercmd; fatload mmc 1:1 ${loadaddr} ${bpi}/${board}/${service}/${kernel}; bootr\0" \
+	SCRIPT_BOOT
+
+
+/* Ethernet */
+#define CONFIG_IPADDR			192.168.1.1
+#define CONFIG_SERVERIP			192.168.1.2
+
+#define CONFIG_SYS_MMC_ENV_DEV		0
+//#define CONFIG_ENV_OFFSET		0x100000
+
+
+#ifndef CONFIG_SPL_BUILD
+
+/*
+ * 160M RAM (256M minimum minus 64MB heap + 32MB for u-boot, stack, fb, etc.
+ * 32M uncompressed kernel, 16M compressed kernel, 1M fdt,
+ * 1M script, 1M pxe and the ramdisk at the end.
+	"bootm_size=0x10000000\0"
+ */
+#define BOOTM_SIZE     __stringify(0xa000000)
+
+#define KERNEL_ADDR_R  __stringify(DSTADDR_KERNEL)
+#define FDT_ADDR_R     __stringify(DSTADDR_DTB)
+#define SCRIPT_ADDR_R  __stringify(0x1000000)
+#define PXEFILE_ADDR_R __stringify(DSTADDR_ROOTFS)
+#define RAMDISK_ADDR_R __stringify(DSTADDR_ROOTFS)
+
+#define MEM_LAYOUT_ENV_SETTINGS \
+	"bootm_size=" BOOTM_SIZE "\0" \
+	"kernel_addr_r=" KERNEL_ADDR_R "\0" \
+	"fdt_addr_r=" FDT_ADDR_R "\0" \
+	"fdtaddr=" FDT_ADDR_R "\0" \
+	"scriptaddr=" SCRIPT_ADDR_R "\0" \
+	"pxefile_addr_r=" PXEFILE_ADDR_R "\0" \
+	"ramdisk_addr_r=" RAMDISK_ADDR_R "\0"
+
+#define DFU_ALT_INFO_RAM \
+	"dfu_alt_info_ram=" \
+	"kernel ram " KERNEL_ADDR_R " 0x1000000;" \
+	"fdt ram " FDT_ADDR_R " 0x100000;" \
+	"ramdisk ram " RAMDISK_ADDR_R " 0x4000000\0"
+
+#ifdef CONFIG_MMC
+#if CONFIG_MMC_SUNXI_SLOT_EXTRA != -1
+#define BOOTENV_DEV_MMC_AUTO(devtypeu, devtypel, instance)		\
+	BOOTENV_DEV_MMC(MMC, mmc, 0)					\
+	BOOTENV_DEV_MMC(MMC, mmc, 1)					\
+	"bootcmd_mmc_bpi=set devnum ${mmc_bootdev}; run mmc_boot;\0"	\
+	"bootcmd_mmc0=set devnum 0; run fdtload2; run fdtload; run mmc_boot;\0"			\
+	"bootcmd_mmc1=set devnum 1; run fdtload2; run fdtload; run mmc_boot;\0"			\
+	"bootcmd_mmc_auto="						\
+		"if test ${mmc_bootdev} -eq 1; then "			\
+			"run bootcmd_mmc1; "				\
+			"run bootcmd_mmc0; "				\
+		"elif test ${mmc_bootdev} -eq 0; then "			\
+			"run bootcmd_mmc0; "				\
+			"run bootcmd_mmc1; "				\
+		"fi\0"
+
+#define BOOTENV_DEV_NAME_MMC_AUTO(devtypeu, devtypel, instance) \
+	"mmc_auto "
+
+#define BOOT_TARGET_DEVICES_MMC(func) func(MMC_AUTO, mmc_auto, na)
+#else
+#define BOOT_TARGET_DEVICES_MMC(func) func(MMC, mmc, 0)
+#endif
+#else
+#define BOOT_TARGET_DEVICES_MMC(func)
+#endif
+
+#ifdef CONFIG_AHCI
+#define BOOT_TARGET_DEVICES_SCSI(func) func(SCSI, scsi, 0)
+#else
+#define BOOT_TARGET_DEVICES_SCSI(func)
+#endif
+
+#ifdef CONFIG_USB_STORAGE
+#define BOOT_TARGET_DEVICES_USB(func) func(USB, usb, 0)
+#else
+#define BOOT_TARGET_DEVICES_USB(func)
+#endif
+
+#ifdef CONFIG_CMD_PXE
+#define BOOT_TARGET_DEVICES_PXE(func) func(PXE, pxe, na)
+#else
+#define BOOT_TARGET_DEVICES_PXE(func)
+#endif
+
+#ifdef CONFIG_CMD_DHCP
+#define BOOT_TARGET_DEVICES_DHCP(func) func(DHCP, dhcp, na)
+#else
+#define BOOT_TARGET_DEVICES_DHCP(func)
+#endif
+
+/* FEL boot support, auto-execute boot.scr if a script address was provided */
+#define BOOTENV_DEV_FEL(devtypeu, devtypel, instance) \
+	"bootcmd_fel=" \
+		"if test -n ${fel_booted} && test -n ${fel_scriptaddr}; then " \
+			"echo '(FEL boot)'; " \
+			"source ${fel_scriptaddr}; " \
+		"fi\0"
+#define BOOTENV_DEV_NAME_FEL(devtypeu, devtypel, instance) \
+	"fel "
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(FEL, fel, na) \
+	BOOT_TARGET_DEVICES_MMC(func) \
+	BOOT_TARGET_DEVICES_SCSI(func) \
+	BOOT_TARGET_DEVICES_USB(func) \
+	BOOT_TARGET_DEVICES_PXE(func) \
+	BOOT_TARGET_DEVICES_DHCP(func)
+
+#ifdef CONFIG_OLD_SUNXI_KERNEL_COMPAT
+/* BPI */
+#define BOOTCMD_SUNXI_COMPAT \
+	"bootcmd_sunxi_compat=" \
+		"setenv root /dev/mmcblk0p2 rootwait; " \
+		"if fatload mmc 0 0x44000000 ${bpi}/${board}/${service}/uEnv.txt; then " \
+			"echo Loaded environment from uEnv.txt; " \
+			"env import -t 0x44000000 ${filesize}; " \
+		"fi; " \
+		"setenv root /dev/mmcblk0p3 rootwait; " \
+		"if ext2load mmc 0 0x44000000 uEnv.txt; then " \
+			"echo Loaded environment from uEnv.txt; " \
+			"env import -t 0x44000000 ${filesize}; " \
+		"fi; " \
+		"setenv bootargs console=${console} root=${root} ${extraargs}; " \
+		"ext2load mmc 0 0x43000000 script.bin && " \
+		"ext2load mmc 0 0x48000000 uImage && " \
+		"bootm 0x48000000\0"
+#else
+#define BOOTCMD_SUNXI_COMPAT
+#endif
+
+#include <config_distro_bootcmd.h>
+
+#ifdef CONFIG_USB_KEYBOARD
+#define CONSOLE_STDIN_SETTINGS \
+	"preboot=usb start\0" \
+	"stdin=serial,usbkbd\0"
+#else
+#define CONSOLE_STDIN_SETTINGS \
+	"stdin=serial\0"
+#endif
+
+#ifdef CONFIG_VIDEO
+#define CONSOLE_STDOUT_SETTINGS \
+	"stdout=serial,vga\0" \
+	"stderr=serial,vga\0"
+#elif CONFIG_DM_VIDEO
+#define CONSOLE_STDOUT_SETTINGS \
+	"stdout=serial,vidconsole\0" \
+	"stderr=serial,vidconsole\0"
+#else
+#define CONSOLE_STDOUT_SETTINGS \
+	"stdout=serial\0" \
+	"stderr=serial\0"
+#endif
+
+#ifdef CONFIG_MTDIDS_DEFAULT
+#define SUNXI_MTDIDS_DEFAULT \
+	"mtdids=" CONFIG_MTDIDS_DEFAULT "\0"
+#else
+#define SUNXI_MTDIDS_DEFAULT
+#endif
+
+#ifdef CONFIG_MTDPARTS_DEFAULT
+#define SUNXI_MTDPARTS_DEFAULT \
+	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0"
+#else
+#define SUNXI_MTDPARTS_DEFAULT
+#endif
+
+#define PARTS_DEFAULT \
+	"name=loader1,start=8k,size=32k,uuid=${uuid_gpt_loader1};" \
+	"name=loader2,size=984k,uuid=${uuid_gpt_loader2};" \
+	"name=esp,size=128M,bootable,uuid=${uuid_gpt_esp};" \
+	"name=system,size=-,uuid=${uuid_gpt_system};"
+
+#define UUID_GPT_ESP "c12a7328-f81f-11d2-ba4b-00a0c93ec93b"
+
+#ifdef CONFIG_ARM64
+#define UUID_GPT_SYSTEM "b921b045-1df0-41c3-af44-4c6f280d3fae"
+#else
+#define UUID_GPT_SYSTEM "69dad710-2ce4-4e3c-b16c-21a1d49abed3"
+#endif
+
+#define CONSOLE_ENV_SETTINGS \
+	CONSOLE_STDIN_SETTINGS \
+	CONSOLE_STDOUT_SETTINGS
+
+#ifdef CONFIG_ARM64
+#define FDTFILE "allwinner/" CONFIG_DEFAULT_DEVICE_TREE ".dtb"
+#else
+#define FDTFILE CONFIG_DEFAULT_DEVICE_TREE ".dtb"
+#endif
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	SUNPLUS_ENV_SETTINGS \
+	CONSOLE_ENV_SETTINGS \
+	MEM_LAYOUT_ENV_SETTINGS \
+	DFU_ALT_INFO_RAM \
+	"fdtfile=" FDTFILE "\0" \
+	"console=ttyS0,115200\0" \
+	SUNXI_MTDIDS_DEFAULT \
+	SUNXI_MTDPARTS_DEFAULT \
+	"uuid_gpt_esp=" UUID_GPT_ESP "\0" \
+	"uuid_gpt_system=" UUID_GPT_SYSTEM "\0" \
+	"partitions=" PARTS_DEFAULT "\0" \
+	BOOTCMD_SUNXI_COMPAT \
+	BOOTENV
+
+#else /* ifndef CONFIG_SPL_BUILD */
+#define CONFIG_EXTRA_ENV_SETTINGS
+#endif
+
 
 #endif /* __CONFIG_PENTAGRAM_H */
